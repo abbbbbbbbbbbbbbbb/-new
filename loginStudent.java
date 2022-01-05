da676c6392a6cdc2ab399bcf1df03d4e155dfef2
cccYuTing <96467412+cccYuTing@users.noreply.github.com>
2022-01-05 15:41:06 +0800

    Add files via upload

--- loginStudent.java
+++ loginStudent.java
@@ -2,13 +2,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 public class loginStudent{
+	private static Object userText;
 	public void loginStudent(JPanel panel_) {		
 		JFrame frame = new JFrame("Login Student");
 		frame.setSize(600, 400);
@@ -17,7 +12,6 @@ public class loginStudent{
 		frame.add(panel);
 		panel.setLayout(null);
 		frame.setVisible(true);
-
 
 		JLabel userLabel = new JLabel("帳號:");
 		userLabel.setBounds(160,20,80,25);                        //setBounds(x, y, width, height)
@@ -42,25 +36,45 @@ public class loginStudent{
 		JButton loginButton = new JButton("登入");               //登入按鈕
 		loginButton.setBounds(250, 100, 80, 25);
 		panel.add(loginButton);
-		loginButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				subject A03 = new subject();
-				A03.subject(panel);
-				frame.dispose();
-			}
-		});
-		
+
+
 		JButton loginButton1 = new JButton("上一頁");       
-        loginButton1.setBounds(10, 10, 96, 23);
-        panel.add(loginButton1);
-        loginButton1.addActionListener(new ActionListener() {
+		loginButton1.setBounds(10, 10, 96, 23);
+		panel.add(loginButton1);
+		loginButton1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				login login = new login();
 				login.identity(panel);
 				frame.dispose();
 			}
+		}); 
+		boolean have = false;
+		ReadData data = new ReadData();
+		String User = userText.getText();
+		System.out.println("A"+User);
+		String Password = passwordText.getText();
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for(int i = 0; i < ReadData.count; i++) {
+					System.out.println("!");
+					if(User.equals(ReadData.user[i]) && Password.equals(ReadData.password[i])) {
+						System.out.println("!!");
+						//						loginButton.addActionListener(new ActionListener() {
+						//							public void actionPerformed(ActionEvent e) {
+						subject A03 = new subject();
+						A03.subject(panel);
+						frame.dispose();
+					}
+					else
+						loginButton.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								JOptionPane.showMessageDialog(null,"帳密錯誤","error",JOptionPane.YES_NO_OPTION);
+							}
+						});
+				}
+			}
+
 		});
 	}
-
 }
 
